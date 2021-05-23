e462e944d8dec5a2db4854719a991ec7e3c61250
LeandroTello <leandrotello1632@gmail.com>
2021-05-23 20:20:53 -0300

    Commit inicial del proyecto

---
+++ src/test/java/com/TPOO2/Tpoo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPOO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpoo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
