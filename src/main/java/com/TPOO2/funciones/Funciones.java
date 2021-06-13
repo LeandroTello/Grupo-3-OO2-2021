1b79be07ea5cb19c7442e70cbad9570d90c5e231
LeandroTello <leandrotello1632@gmail.com>
2021-06-13 13:31:51 -0300

    Se cambio el formato de la fecha en la generacion del URL

--- src/main/java/com/TPOO2/funciones/Funciones.java
+++ src/main/java/com/TPOO2/funciones/Funciones.java
@@ -20,8 +20,6 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-
-
 public class Funciones {
 
 	public static String encriptarPass(String pass) {
@@ -41,24 +39,28 @@ public class Funciones {
 
 	public static String generarUrlPeriodo(PermisoPeriodoModel permisoPeriodoModel){
         LocalDate fin= permisoPeriodoModel.getFecha();
+        LocalDate inicio= permisoPeriodoModel.getFecha();
         fin= fin.plusDays(permisoPeriodoModel.getCantDias());
         String fechaFin=fin.getDayOfMonth()+"-"+fin.getMonthValue()+"-"+fin.getYear();
+        String fechaInicio = inicio.getDayOfMonth()+"-"+inicio.getMonthValue()+"-"+inicio.getYear();
         List<LugarModel> desdeHasta = new ArrayList<LugarModel>(permisoPeriodoModel.getDesdeHasta());
         return "nombre="+permisoPeriodoModel.getPedido().getNombre()+"&apellido="+permisoPeriodoModel.getPedido().getApellido()+"&dni="+permisoPeriodoModel.getPedido().getDni()+
                 "&desde="+desdeHasta.get(0).getLugar()+"&desdeCP="+desdeHasta.get(0).getCodigoPostal()+
                 "&hasta="+desdeHasta.get(1).getLugar()+"&hastaCP="+desdeHasta.get(1).getCodigoPostal()
-                +"&inicio="+permisoPeriodoModel.getFecha()+"&fin="+fechaFin
+                +"&inicio="+fechaInicio+"&fin="+fechaFin
                 +"&vehiculo="+permisoPeriodoModel.getRodado().getVehiculo()+"&dominio="+permisoPeriodoModel.getRodado().getDominio()+"&vacaciones="+permisoPeriodoModel.isVacaciones();
 
     }
 
 
     public static String generarUrlDiario(PermisoDiarioModel permisoDiarioModel) {
+        LocalDate inicio= permisoDiarioModel.getFecha();
+        String fechaInicio = inicio.getDayOfMonth()+"-"+inicio.getMonthValue()+"-"+inicio.getYear();
         List<LugarModel> desdeHasta = new ArrayList<LugarModel>(permisoDiarioModel.getDesdeHasta());
         return "nombre=" + permisoDiarioModel.getPedido().getNombre() + "&apellido="
                 + permisoDiarioModel.getPedido().getApellido() + "&dni=" + permisoDiarioModel.getPedido().getDni()
                 + "&desde=" + desdeHasta.get(0).getLugar() + "&desdeCP=" + desdeHasta.get(0).getCodigoPostal()
                 + "&hasta=" + desdeHasta.get(1).getLugar() + "&hastaCP=" + desdeHasta.get(1).getCodigoPostal()
-                + "&inicio=" + permisoDiarioModel.getFecha()+"&motivo="+permisoDiarioModel.getMotivo();
+                + "&inicio=" + fechaInicio +"&motivo="+permisoDiarioModel.getMotivo();
     }
 }
